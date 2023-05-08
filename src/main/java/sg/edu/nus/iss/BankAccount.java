44f0b3ba9df59edc8f87ea0738f9b71fdbfc19f4
antsimch <anthonysimch@gmail.com>
2023-05-08 14:53:31 +0800

    BankAccount

---
+++ src/main/java/sg/edu/nus/iss/BankAccount.java
@@ -0,0 +1,112 @@
+package sg.edu.nus.iss;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankAccount {
+    private final String name;
+    private final String accNumber;
+    private float balance;
+    private List<String> transactions;
+    private boolean closed;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private static int num = 12345;
+
+    public BankAccount(String name) {
+        this(name, 0);
+    }
+
+    public BankAccount(String name, float balance) {
+        this.name = name;
+        this.balance = balance;
+        this.accNumber = "00" + num;
+        num++;
+        this.transactions = new ArrayList<>();
+        this.closed = false;
+        this.startDate = LocalDate.now();
+    }
+
+    public void deposit(int amount) {
+        if (isClosed()) {
+            throw new IllegalArgumentException("Account is closed\n");
+        } 
+        
+        if (amount > 0) {
+            String depositMsg = "deposit $" + amount + " at " + LocalDateTime.now();
+            setBalance(getBalance() + amount);
+            transactions.add(depositMsg);
+            System.out.println(depositMsg);
+        } else {
+            throw new IllegalArgumentException("Deposit only accepts positive amount\n");
+        }
+    }
+
+    public void withdraw(int amount) {
+        if (isClosed()) {
+            throw new IllegalArgumentException("Account is closed\n");
+        } 
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdraw only accepts positive amount\n");
+        }
+
+        if ((getBalance() - amount) >= 0) {
+            String withdrawMsg = "withdraw $" + amount + " at " + LocalDateTime.now();
+            setBalance(getBalance() - amount);
+            transactions.add(withdrawMsg);
+            System.out.println(withdrawMsg);
+        } else {
+            throw new IllegalArgumentException("Amount is greater than current balance\n");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount [name=" + name + ", accNumber=" + accNumber + ", balance=" + balance + ", transactions="
+                + transactions + ", closed=" + closed + ", startDate=" + startDate + ", endDate=" + endDate + "]";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public List<String> getTransactions() {
+        return transactions;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+        this.setEndDate(LocalDate.now()); 
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
--- src/main/java/sg/edu/nus/iss/Main.java
+++ src/main/java/sg/edu/nus/iss/Main.java
@@ -8,6 +8,23 @@ public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        BankAccount john = new BankAccount("John", 2050);
+        BankAccount tom = new BankAccount("Tom", 2025);
+        BankAccount ben = new BankAccount("Ben");
+
+        System.out.println(john.getAccNumber());
+        System.out.println(tom.getAccNumber());
+        System.out.println(ben.getAccNumber());
+
+        john.deposit(2000);
+        john.withdraw(1000);
+        // tom.setClosed(true);
+        tom.withdraw(1000);
+        ben.deposit(1000);
+        // ben.withdraw(1500);
+
+        System.out.println(john.getTransactions());
+        System.out.println(tom.getTransactions());
+        System.out.println(ben.getTransactions());
     }
 }
